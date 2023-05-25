a38be121df4a7ecd58b40ba68a9e20cd80c5679a
Rahul Bhagwat <72269385+rahulbhagwat28@users.noreply.github.com>
2023-05-25 14:03:18 -0500

    added JPA entities for the system

--- src/main/java/com/SpringBasics/EmployeeSystem/Entities/Manager.java
+++ src/main/java/com/SpringBasics/EmployeeSystem/Entities/Manager.java
@@ -19,6 +19,7 @@ public class Manager {
 
     private String manager_name;
 
+    //One manager can have multiple employees under him
     @OneToMany(cascade = CascadeType.PERSIST,fetch=FetchType.EAGER,mappedBy="manager")
    private Collection<Employee> employees;
 
